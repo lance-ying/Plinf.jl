2b0aa000191a089a3323111820cf2143375a3bdb
Xuan <tanqazx@gmail.com>
2023-09-25 15:53:44 -0400

    Reorganize utilities.

--- domains/Assistive/render.jl
+++ domains/Assistive/render.jl
@@ -3,9 +3,7 @@ using PDDL, SymbolicPlanners
 using PDDLViz, GLMakie
 using Test
 
-using Makie.FFMPEG
 import Makie: to_color
-import PDDLViz: Animation, is_displayed
 
 # Define colors
 vibrant = PDDLViz.colorschemes[:vibrant]
@@ -130,3 +128,52 @@ renderer_labeled_doors = PDDLViz.GridworldRenderer(
         :object_colors => [:black, :slategray]
     )
 )
+
+"Adds a subplot to a storyboard with a line plot of goal probabilities."
+function storyboard_goal_lines!(
+    storyboard::Figure, goal_probs, ts=Int[];
+    goal_names = ["(has gem1)", "(has gem2)", "(has gem3)", "(has gem4)"],
+    goal_colors = PDDLViz.colorschemes[:vibrant][1:length(goal_names)],
+    show_legend = false, ts_linewidth = 1, ts_fontsize = 24, 
+    ax_args = (), kwargs...
+)
+    n_rows, n_cols = size(storyboard.layout)
+    width, height = size(storyboard.scene)
+    # Add goal probability subplot
+    if length(ts) == size(goal_probs)[2]
+        curves = [[Makie.Point2f(t, p) for (t, p) in zip(ts, goal_probs[i, :])]
+                  for i in 1:size(goal_probs)[1]]
+        ax, _ = series(
+            storyboard[n_rows+1, 1:n_cols], curves;
+            color = goal_colors, labels=goal_names,
+            axis = (xlabel="Time", ylabel = "Probability",
+                    limits=((1, ts[end]), (0, 1)), ax_args...),
+            kwargs...
+        )
+    else
+        ax, _ = series(
+            storyboard[n_rows+1, 1:n_cols], goal_probs,
+            color = goal_colors, labels=goal_names,
+            axis = (xlabel="Time", ylabel = "Probability",
+                    limits=((1, size(goal_probs, 2)), (0, 1)), ax_args...),
+            kwargs...
+        )
+    end
+    # Add legend to subplot
+    if show_legend
+        axislegend("Goals", framevisible=false)
+    end
+    # Add vertical lines at timesteps
+    if !isempty(ts)
+        vlines!(ax, ts, color=:black, linestyle=:dash,
+                linewidth=ts_linewidth)
+        positions = [(t + 0.1, 0.85) for t in ts]
+        labels = ["t = $t" for t in ts]
+        text!(ax, positions; text=labels, color = :black,
+              fontsize=ts_fontsize)
+    end
+    # Resize figure to fit new plot
+    rowsize!(storyboard.layout, n_rows+1, Auto(0.25))
+    resize!(storyboard, (width, height * 1.3))
+    return storyboard
+end
